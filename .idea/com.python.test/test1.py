ccc3b040db8b7ae95fca9aed9f59cbecdd817926
王聪 <email@example.com>
2020-09-06 18:30:30 +0800

    pymysql操作

--- .idea/com.python.test/test1.py
+++ .idea/com.python.test/test1.py
@@ -3,30 +3,42 @@ import pymysql  ##引入依赖包
 
 def main():
     connect = pymysql.connect(host='127.0.0.1', port=3306, user='root',
-                              password='cong', db='my_test', charset='utf8')
+                              password='cong', db='my_test', charset='utf8',
+                              cursorclass=pymysql.cursors.DictCursor) #使用字典游标查询
 
     # 插入操作
-    try:
-        with connect.cursor() as cursor:
-            # 拿到游标后就可以执行sql
-            result = cursor.execute("insert into employee values (11,'王聪a',123,'dada@qq.com');")
-            if result == 1:
-                print("插入成功！")
+    # try:
+    #     with connect.cursor() as cursor:
+    #         # 拿到游标后就可以执行sql
+    #         result = cursor.execute("insert into employee values (11,'王聪a',123,'dada@qq.com');")
+    #         if result == 1:
+    #             print("插入成功！")
+    #
+    #         connect.commit()  # 需要提交事务
+    # except: pymysql.MySQLError:  connect.rollback()
+    #
+    #
+    # # 更新操作
+    # id = int(input("输入id"))
+    # lastName = input("输入名称")
+    # with connect.cursor() as cursor2:
+    #     cursor2.execute('update employee set id = %s ,last_name = %s',(id,lastName))
+    # connect.commit()
 
-            connect.commit()  # 需要提交事务
-    except: pymysql.MySQLError:  connect.rollback()
 
-
-    # 更新操作
-    id = int(input("输入id"))
-    lastName = input("输入名称")
-    with connect.cursor() as cursor2:
-        cursor2.execute('update employee set id = %s ,last_name = %s',(id,lastName))
-    connect.commit()
-
-    # 插叙操作
+    # 查询操作
     with connect.cursor() as cursor1:
-        set = cursor1.execute("select * from employee")
+        cursor1.execute("select id id,last_name name,gender gen ,email from employee")
+        #插叙所有行
+        result = cursor1.fetchall()
+        for row in result:
+            print(row['id'] ,row['name'] , row['gen'] , row['email'])
+        # print(f'{row[0]} {row[1]} {row[2]} {row[3]}')
+        #查询5行
+        #cursor1.fetchmany(5)
+        #查询一行
+        #cursor1.fetchone()
+
     connect.commit()
     # print(connect)
 
